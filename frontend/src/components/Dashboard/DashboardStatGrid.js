605db2d47bfac24254f56fe0acc25efe0e04add6
prathila01 <prathila01@gmail.com>
2023-10-12 02:13:58 +0530

    Dashboard reach count table

--- frontend/src/components/Dashboard/DashboardStatGrid.js
+++ frontend/src/components/Dashboard/DashboardStatGrid.js
@@ -3,12 +3,14 @@ import axios from 'axios'
 import { IoBagHandle, IoPieChart, IoPeople, IoCart } from 'react-icons/io5'
 import { Pie } from 'react-chartjs-2'
 import PieChartComponent from './PieChartComponent'
+import * as XLSX from 'xlsx'
 
 export default function DashboardStatGrid() {
   const [customers, SetCustomers] = useState([])
   const [orders, SetOrders] = useState([])
   const [items, SetItems] = useState([])
   const [categoryReach, SetCategoryReach] = useState([])
+  const [itemReachs, SetItemReach] = useState([])
 
   const [data, setData] = useState({
     categoryReachObject: [],
@@ -29,27 +31,19 @@ export default function DashboardStatGrid() {
     getCustomers()
   }, [])
 
-  // useEffect(() => {
-  //   // Function to fetch data from the API
-  //   const axios = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         'http://localhost:4000/api/v1/category-reach'
-  //       )
-  //       const result = await response.json().categoryReachObject
-
-  //       if (result && Array.isArray(result)) {
-  //         setData({ result })
-  //         console.log(data)
-  //       }
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error)
-  //     }
-  //   }
-
-  //   // Call the fetch function
-  //   fetchData()
-  // }, [])
+  useEffect(() => {
+    function getItemReach() {
+      axios
+        .get('http://localhost:4000/api/v1/item-reach')
+        .then((res) => {
+          SetItemReach(res.data.recommendations)
+        })
+        .catch((err) => {
+          alert(err.message)
+        })
+    }
+    getItemReach()
+  }, [])
 
   useEffect(() => {
     function getCategoryReach() {
@@ -65,18 +59,6 @@ export default function DashboardStatGrid() {
 
     getCategoryReach()
   }, [])
-
-  // const getCategoryReach = () => {
-  //   axios
-  //     .get(`http://localhost:4000/api/v1/category-reach`)
-  //     .then((res) => {
-  //       console.log(res.data)
-  //       SetCategoryReach(res.data)
-  //     })
-  //     .catch((err) => {
-  //       alert(err.message)
-  //     })
-  // }
 
   useEffect(() => {
     function getOrders() {
@@ -107,6 +89,13 @@ export default function DashboardStatGrid() {
 
     getItems()
   }, [])
+
+  const exportToExcel = () => {
+    const ws = XLSX.utils.json_to_sheet(itemReachs)
+    const wb = XLSX.utils.book_new()
+    XLSX.utils.book_append_sheet(wb, ws, 'Item Reached')
+    XLSX.writeFile(wb, 'ItemReached.xlsx')
+  }
 
   return (
     <div>
@@ -178,6 +167,56 @@ export default function DashboardStatGrid() {
         {/* pie chart */}
         <div className="flex justify-center items-center h-screen">
           <PieChartComponent data={data} />
+        </div>
+      </div>
+
+      <div className="bg-white px-4 pt-3 pb-4 rounded-sm border border-gray-200 flex-1">
+        <div className="flex justify-between items-center">
+          <strong className="text-gray-700 font-medium">
+            Top Reached Items
+          </strong>
+
+          <button
+            onClick={exportToExcel}
+            className="bg-blue-500 text-white px-4 py-2 rounded "
+          >
+            Export
+          </button>
+        </div>
+        <div className="border-x border-gray-200 rounded-sm mt-3">
+          <table className="w-full text-gray-700">
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>Item Name</th>
+                <th>Image</th>
+                <th>Category</th>
+                <th>Reach Count</th>
+              </tr>
+            </thead>
+            <tbody>
+              {itemReachs.map((itemReach, index) => (
+                <tr key={itemReach._id}>
+                  <td>{index + 1}</td>
+                  <td>{itemReach.item.item_name}</td>
+                  <td>
+                    <div class="flex items-center">
+                      <div class="flex-shrink-0 h-20 w-20">
+                        <img
+                          class="h-20 w-20 "
+                          crossOrigin="anonymous"
+                          src={itemReach.item.image}
+                          alt=""
+                        ></img>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{itemReach.item.category}</td>
+                  <td>{itemReach.reach_count}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
