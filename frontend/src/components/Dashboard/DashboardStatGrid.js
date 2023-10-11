2144e16ab8a80731432384b557af3c0be6cc1daa
prathila01 <prathila01@gmail.com>
2023-10-12 01:50:17 +0530

    Dashboard pie chart

--- frontend/src/components/Dashboard/DashboardStatGrid.js
+++ frontend/src/components/Dashboard/DashboardStatGrid.js
@@ -10,6 +10,10 @@ export default function DashboardStatGrid() {
   const [items, SetItems] = useState([])
   const [categoryReach, SetCategoryReach] = useState([])
 
+  const [data, setData] = useState({
+    categoryReachObject: [],
+  })
+
   useEffect(() => {
     function getCustomers() {
       axios
@@ -25,62 +29,54 @@ export default function DashboardStatGrid() {
     getCustomers()
   }, [])
 
-  const data = {
-    categoryReachObject: [
-      {
-        reach_count: 16,
-        _id: '6526460c1a03f23958e96453',
-        category: 'Sports',
-        createdAt: '2023-10-11T06:51:56.901Z',
-        updatedAt: '2023-10-11T19:13:13.767Z',
-        __v: 0,
-      },
-      {
-        reach_count: 7,
-        _id: '652645f11a03f23958e96451',
-        category: 'Tshirts',
-        createdAt: '2023-10-11T06:51:29.114Z',
-        updatedAt: '2023-10-11T19:13:15.080Z',
-        __v: 0,
-      },
-      // ... [other objects in the array, similar format as above]
-      {
-        reach_count: 5,
-        _id: '6526466d1a03f23958e96461',
-        category: 'Socks',
-        createdAt: '2023-10-11T06:53:33.575Z',
-        updatedAt: '2023-10-11T06:53:33.575Z',
-        __v: 0,
-      },
-    ],
-  }
-
   // useEffect(() => {
-  //   function getCategoryReach() {
-  //     axios
-  //       .get('http://localhost:4000/api/v1/category-reach')
-  //       .then((res) => {
-  //         SetCategoryReach(res.data)
-  //       })
-  //       .catch((err) => {
-  //         alert(err.message)
-  //       })
+  //   // Function to fetch data from the API
+  //   const axios = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         'http://localhost:4000/api/v1/category-reach'
+  //       )
+  //       const result = await response.json().categoryReachObject
+
+  //       if (result && Array.isArray(result)) {
+  //         setData({ result })
+  //         console.log(data)
+  //       }
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error)
+  //     }
   //   }
 
-  //   getCategoryReach()
+  //   // Call the fetch function
+  //   fetchData()
   // }, [])
 
-  const getCategoryReach = () => {
-    axios
-      .get(`http://localhost:4000/api/v1/category-reach`)
-      .then((res) => {
-        console.log(res.data)
-        SetCategoryReach(res.data)
-      })
-      .catch((err) => {
-        alert(err.message)
-      })
-  }
+  useEffect(() => {
+    function getCategoryReach() {
+      axios
+        .get('http://localhost:4000/api/v1/category-reach')
+        .then((res) => {
+          setData(res.data)
+        })
+        .catch((err) => {
+          alert(err.message)
+        })
+    }
+
+    getCategoryReach()
+  }, [])
+
+  // const getCategoryReach = () => {
+  //   axios
+  //     .get(`http://localhost:4000/api/v1/category-reach`)
+  //     .then((res) => {
+  //       console.log(res.data)
+  //       SetCategoryReach(res.data)
+  //     })
+  //     .catch((err) => {
+  //       alert(err.message)
+  //     })
+  // }
 
   useEffect(() => {
     function getOrders() {
@@ -113,76 +109,76 @@ export default function DashboardStatGrid() {
   }, [])
 
   return (
-    <div onLoad={getCategoryReach}>
-      <div className="flex flex-col gap-4 w-full items-start">
-        <div className="flex gap-4 w-full">
-          <BoxWrapper>
-            <div className="rounded-full h-12 w-12 flex items-center justify-center bg-sky-500">
-              <IoBagHandle className="text-2xl text-white" />
+    <div>
+      <div className="flex gap-4 w-full">
+        <BoxWrapper>
+          <div className="rounded-full h-12 w-12 flex items-center justify-center bg-sky-500">
+            <IoBagHandle className="text-2xl text-white" />
+          </div>
+          <div className="pl-4">
+            <span className="text-sm text-gray-500 font-light">
+              Total Items
+            </span>
+            <div className="flex items-center">
+              <strong className="text-xl text-gray-700 font-semibold">
+                {items.nbHits}
+              </strong>
             </div>
-            <div className="pl-4">
-              <span className="text-sm text-gray-500 font-light">
-                Total Items
-              </span>
-              <div className="flex items-center">
-                <strong className="text-xl text-gray-700 font-semibold">
-                  {items.nbHits}
-                </strong>
-              </div>
+          </div>
+        </BoxWrapper>
+        <BoxWrapper>
+          <div className="rounded-full h-12 w-12 flex items-center justify-center bg-orange-600">
+            <IoPieChart className="text-2xl text-white" />
+          </div>
+          <div className="pl-4">
+            <span className="text-sm text-gray-500 font-light">
+              Total Expenses
+            </span>
+            <div className="flex items-center">
+              <strong className="text-xl text-gray-700 font-semibold">
+                $3423
+              </strong>
+              <span className="text-sm text-green-500 pl-2">-343</span>
             </div>
-          </BoxWrapper>
-          <BoxWrapper>
-            <div className="rounded-full h-12 w-12 flex items-center justify-center bg-orange-600">
-              <IoPieChart className="text-2xl text-white" />
+          </div>
+        </BoxWrapper>
+        <BoxWrapper>
+          <div className="rounded-full h-12 w-12 flex items-center justify-center bg-yellow-400">
+            <IoPeople className="text-2xl text-white" />
+          </div>
+          <div className="pl-4">
+            <span className="text-sm text-gray-500 font-light">
+              Total Customers
+            </span>
+            <div className="flex items-center">
+              <strong className="text-xl text-gray-700 font-semibold">
+                {customers.count}
+              </strong>
             </div>
-            <div className="pl-4">
-              <span className="text-sm text-gray-500 font-light">
-                Total Expenses
-              </span>
-              <div className="flex items-center">
-                <strong className="text-xl text-gray-700 font-semibold">
-                  $3423
-                </strong>
-                <span className="text-sm text-green-500 pl-2">-343</span>
-              </div>
+          </div>
+        </BoxWrapper>
+        <BoxWrapper>
+          <div className="rounded-full h-12 w-12 flex items-center justify-center bg-green-600">
+            <IoCart className="text-2xl text-white" />
+          </div>
+          <div className="pl-4">
+            <span className="text-sm text-gray-500 font-light">
+              Total Orders
+            </span>
+            <div className="flex items-center">
+              <strong className="text-xl text-gray-700 font-semibold">
+                {orders.count}
+              </strong>
             </div>
-          </BoxWrapper>
-          <BoxWrapper>
-            <div className="rounded-full h-12 w-12 flex items-center justify-center bg-yellow-400">
-              <IoPeople className="text-2xl text-white" />
-            </div>
-            <div className="pl-4">
-              <span className="text-sm text-gray-500 font-light">
-                Total Customers
-              </span>
-              <div className="flex items-center">
-                <strong className="text-xl text-gray-700 font-semibold">
-                  {customers.count}
-                </strong>
-              </div>
-            </div>
-          </BoxWrapper>
-          <BoxWrapper>
-            <div className="rounded-full h-12 w-12 flex items-center justify-center bg-green-600">
-              <IoCart className="text-2xl text-white" />
-            </div>
-            <div className="pl-4">
-              <span className="text-sm text-gray-500 font-light">
-                Total Orders
-              </span>
-              <div className="flex items-center">
-                <strong className="text-xl text-gray-700 font-semibold">
-                  {orders.count}
-                </strong>
-              </div>
-            </div>
-          </BoxWrapper>
-        </div>
+          </div>
+        </BoxWrapper>
       </div>
 
-      {/* pie chart */}
-      <div className="w-100 mt-4">
-        <PieChartComponent data={data} />
+      <div>
+        {/* pie chart */}
+        <div className="flex justify-center items-center h-screen">
+          <PieChartComponent data={data} />
+        </div>
       </div>
     </div>
   )
